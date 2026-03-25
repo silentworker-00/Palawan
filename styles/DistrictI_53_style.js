83ddb151ef95760518f89f20ac9e02d5b8276245
silentworker-00 <tolentinomg17@gmail.com>
2026-03-25 09:02:15 +0800

    Add files via upload

---
+++ styles/DistrictI_53_style.js
@@ -0,0 +1,31 @@
+var size = 0;
+var placement = 'point';
+
+var style_DistrictI_53 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("");
+    var labelFont = "13.0px \'Times New Roman\', sans-serif";
+    var labelFill = "#ff0000";
+    var bufferColor = "#fafafa";
+    var bufferWidth = 3.0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if (feature.get("Mun_Name") !== null) {
+        labelText = String(feature.get("Mun_Name"));
+    }
+    var style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: [2.128,4.256], lineCap: 'square', lineJoin: 'bevel', width: 2.128}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
